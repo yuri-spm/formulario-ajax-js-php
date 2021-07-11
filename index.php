814103a43cf250b12f81f42ff92a082a250631c1
Yuri Monte <yuri.spm@gmail.com>
2021-07-11 16:42:48 -0300

    Add bd

--- index.php
+++ index.php
@@ -1,7 +1,3 @@
-<?php
-    var_dump($_GET);
-?>
-
 <!doctype html>
 <html lang="en">
 <head>
@@ -13,58 +9,49 @@
     <link href="node_modules/bootstrap/dist/css/bootstrap-reboot.css" rel="stylesheet">
     <link href="node_modules/bootstrap/dist/css/bootstrap-grid.css" rel="stylesheet">
     <link href="node_modules/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="node_modules/jquery/dist/jquery.js"></script>
 </head>
 
 <body>
-<form action="" method="" id="formulario">
+<form action="" method="post" id="formulario">
     <div class="card">
         <h2 class="card-header">Formulário de cadastro</h2>
     </div>
     <div class="form-group">
-        <label id="name" class="align-content-sm-center">Primeiro Nome</label>
-        <input type="text" class="form-control" name="first_name">
+        <label  class="align-content-sm-center">Nome Completo</label>
+        <input type="text" class="form-control" name="name">
     </div>
     <div class="form-group">
-        <label id="telefone" class="align-content-sm-center">Telefone</label>
+        <label  class="align-content-sm-center">Telefone</label>
         <input type="text" class="form-control" name="phone">
     </div>
     <div class="form-group">
-        <label id="email">Email</label>
-        <input type="text" class="form-control"  name="email" >
+        <label >Email</label>
+        <input type="email" class="form-control"  name="email" >
     </div>
-    <button   class="align-content-sm-center btn btn-primary">Enviar</button>
+    <button  id="enviar" class="align-content-sm-center btn btn-primary">Enviar</button>
 
     <script>
 
         $(function() {
-
-
-            $('#formulario').click(function() {
-
-                $.ajax({
-                    url: 'index.php',
-                    type: 'post',
-                    data: {'first_name' : $('#first_name').val(),
-                        'phone' : $('phone').val(),
-                        'email' : $('email').val()
-                    },
-
-                    success: function(response) {
-                        console.log(response);
-                        if (response){
-                            var json = JSON.parse(response);
-
+            $('#formulario').submit(function () {
+               $.ajax({
+                   url: "insert.php",
+                   type: 'POST',
+                   data: $('#formulario').serialize(),
+                   success: function (data) {
+                        if ($('#formulario') !== ''){
+                            alert("Formulario enviado com sucesso.")
+                            return false;
+                        } else {
+                            alert("Preencha os dados");
 
                         }
-
-                    },
-                    error: function(ex, hre) {
-                        console.log()
-                    }
-
-                })
-
-            })
+               }
+            });
+        return false;
+    });
+});
 
 
     </script>
